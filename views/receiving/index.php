bd0a1a275e432b22cf4a006d1b495baaf7d108e7
Alex John Suarez <alexjohnsuarez@gmail.com>
2015-02-07 15:00:24 +0800

    modified popup box

--- views/receiving/index.php
+++ views/receiving/index.php
@@ -144,7 +144,7 @@ data-toggle="modal"
         <div class="control-group">
             <?= Html::textInput('open_pallet_no', '', ['id'		 => 'open-pallet-no',
 								 					  	'class'	 	 => 'uborder help-40percent']) ?>
-            <?= Html::submitButton('Use Pallet', ['class' 	=> 'btn',
+            <?= Html::submitButton('Use Pallet', ['class' 	=> 'btn btn-success',
         									  	  'name'	=> 'open-pallet']) ?>
         </div>
       <?php ActiveForm::end(); ?>
@@ -157,7 +157,7 @@ data-toggle="modal"
 
 
 <!-- Close Pallet -->
-<div id="closepallet" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div style="height:230px" id="closepallet" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
     <h3 id="myModalLabel" class="header-popup">Close Pallet</h3>
@@ -175,7 +175,7 @@ data-toggle="modal"
         <div class="control-group">
             <?= Html::textInput('close_pallet_no', '', ['id'		 => 'close-pallet-no',
 								 					  	'class'	 	 => 'uborder help-40percent']) ?>
-            <?= Html::submitButton('Use Pallet', ['class' 	=> 'btn',
+            <?= Html::submitButton('Use Pallet', ['class' 	=> 'btn btn-success',
         									  	  'name'	=> 'close-pallet']) ?>
         </div>
       <?php ActiveForm::end(); ?>
@@ -188,7 +188,7 @@ data-toggle="modal"
 
 
 <!-- Reject Pallet -->
-<div id="rejectpallet" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div style="height:230px" id="rejectpallet" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
     <h3 id="myModalLabel" class="header-popup">Reject Pallet</h3>
@@ -206,7 +206,7 @@ data-toggle="modal"
         <div class="control-group">
             <?= Html::textInput('reject_pallet_no', '', ['id'		 => 'reject-pallet-no',
 								 					  	'class'	 	 => 'uborder help-40percent']) ?>
-            <?= Html::submitButton('Use Pallet', ['class' 	=> 'btn',
+            <?= Html::submitButton('Use Pallet', ['class' 	=> 'btn btn-success',
         									  	  'name'	=> 'reject-pallet']) ?>
         </div>
       <?php ActiveForm::end(); ?>
@@ -219,7 +219,7 @@ data-toggle="modal"
 
 
 <!-- Create Transfer Order -->
-<div id="createto" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div style="height:230px" id="createto" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
     <h3 id="myModalLabel" class="header-popup">Create Transfer Order</h3>
@@ -229,7 +229,7 @@ data-toggle="modal"
       <form action="receiving_menu.php" method="post" class="form-horizontal" role="form">
         <div class="control-group">
             <input type="text" class="uborder help-40percent" />
-            <button class="btn" data-dismiss="modal" aria-hidden="true">Use Pallet</button>
+            <button class="btn btn-success" data-dismiss="modal" aria-hidden="true">Use Pallet</button>
         </div>
       </form>
     
