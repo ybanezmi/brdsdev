95b6026aaada55a8cdcd20c40b52ef25a51ffc54
Alex John Suarez <alexjohnsuarez@gmail.com>
2015-03-15 04:47:12 +0800

    fixed reset button

--- views/admin-tools/synchronized-materials.php
+++ views/admin-tools/synchronized-materials.php
@@ -2,6 +2,10 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+/* @var $this yii\web\View */
+/* @var $model app\models\TrxTransactions */
+/* @var $customer_list app\models\MstCustomer */
+/* @var $form yii\widgets\ActiveForm */
 $this->title = 'Synch Materials';
 ?>
 
@@ -10,7 +14,9 @@ $this->title = 'Synch Materials';
 		<div class="wrapper-150">
 			<h1 class="page-title">Synch Materials</h1>
 			<div class="one-column help-bg-gray pdt-one-column" style="width:50%" >
-   <?php 
+
+       <?php 
+    	$js = 'function beforeValidate(form) {if ( form.data("back") {this.validateOnSubmit = false;this.beforeValidate = "";form.submit();return false;}return true;}';
     	$form = ActiveForm::begin([
     	'fieldConfig' => [
     		'template' => '<div class="control-group">{label}<div class="f-full-size">{input}</div><div class=\"col-lg-8\">{error}</div></div>',
@@ -28,10 +34,11 @@ $this->title = 'Synch Materials';
 			
     	<div class="one-column-button pdt-one-column-button" style="wdith:100%;">
 			<div class="submit-button ie6-submit-button">
-				<button class="btn btn-primary" style="width:35%;" name="synch_all">Sync All Materials</button>
+				<button type="submit" class="btn btn-primary" style="width:35%;" name="synch_all">Sync All Materials</button>
 				<button class="btn btn-primary" style="width:35%;">View Details</button>
 				<button class="btn btn-primary" style="width:35%;">Material Only</button>
-				<button class="btn btn-primary" style="width:35%;" name="back">Back To Main</button>
+				<button type="submit" class="btn btn-primary back cancel-button" style="width:35%;" name="back">Back To Main</button>
+
         	</div>
         </div>
 
