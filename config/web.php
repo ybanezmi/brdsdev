c5d59dfac7322ce7638bde851af4d1ca03e8a3d0
Michelle Ybañez <ybanez.mich@gmail.com>
2015-05-04 11:56:33 +0800

    [SAP] Initial commit.

--- config/web.php
+++ config/web.php
@@ -68,6 +68,9 @@ $config = [
 		'dateFormatter' => [
 			'class' => 'app\components\CDateFormatter',
 		],
+		'sapRfc' => [
+			'class' => 'app\components\CSapRfc',
+		],
 		'response' => [
             'formatters' => [
                 'pdf' => [
@@ -103,7 +106,7 @@ $config = [
         'DNS1D' => 'Dinesh\Barcode\DNS1D',
 		'DNS2D' => 'Dinesh\Barcode\Facades\DNS2DFacade',
     ],
-    'aliases' => [	
+    'aliases' => [
     				'DNS1D' => 'Dinesh\Barcode\Facades\DNS1DFacade',
         			'DNS2D' => 'Dinesh\Barcode\Facades\DNS2DFacade',
         		 ],
@@ -116,40 +119,40 @@ $config = [
             ],
 		'datecontrol' =>  [
 	        'class' => 'kartik\datecontrol\Module',
-	
+
 	        // format settings for displaying each date attribute (ICU format example)
 	        'displaySettings' => [
 	            Module::FORMAT_DATE => 'MM/dd/yyyy',
 	            Module::FORMAT_TIME => 'HH:mm:ss a',
-	            Module::FORMAT_DATETIME => 'MM/dd/yyyy HH:mm:ss a', 
+	            Module::FORMAT_DATETIME => 'MM/dd/yyyy HH:mm:ss a',
 	        ],
-	
+
 	        // format settings for saving each date attribute (PHP format example)
 	        'saveSettings' => [
 	            Module::FORMAT_DATE => 'php:Y-m-d', // saves as unix timestamp
 	            Module::FORMAT_TIME => 'php:H:i:s',
 	            Module::FORMAT_DATETIME => 'php:Y-m-d H:i:s',
 	        ],
-	
+
 	        // set your display timezone
 	        'displayTimezone' => 'Asia/Manila',
-	
+
 	        // set your timezone for date saved to db
 	        'saveTimezone' => 'Asia/Manila',
-	
+
 	        // automatically use kartik\widgets for each of the above formats
 	        'autoWidget' => true,
-	
+
 	        // use ajax conversion for processing dates from display format to save format.
 	        'ajaxConversion' => true,
-	
+
 	        // default settings for each widget from kartik\widgets used when autoWidget is true
 	        'autoWidgetSettings' => [
 	            Module::FORMAT_DATE => ['type'=>2, 'pluginOptions'=>['autoclose'=>true]], // example
 	            Module::FORMAT_DATETIME => [], // setup if needed
 	            Module::FORMAT_TIME => [], // setup if needed
 	        ],
-	
+
 	        // custom widget settings that will be used to render the date input instead of kartik\widgets,
 	        // this will be used when autoWidget is set to false at module or widget level.
 	        'widgetSettings' => [
@@ -172,7 +175,7 @@ $config = [
 	'extensions' => [
 		'vendor\barcodegenerator\BarcodeGeneratorController',
 	],
-    
+
 ];
 
 if (YII_ENV_DEV) {
---
+++ constants/SapConst.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace app\constants;
+
+class SapConst
+{
+    // common
+    const EMPTY_STRING = '';
+    const HALF_WIDTH_SPACE = ' ';
+
+    // config
+    const ASHOST = 'ASHOST';
+    const SYSNR = 'SYSNR';
+    const CLIENT = 'CLIENT';
+    const USER = 'USER';
+    const PASSWD = 'PASSWD';
+    const LANG = 'LANG';
+
+    // functions
+    const ZBAPI_RECEIVING = 'ZBAPI_RECEIVING';
+
+    // RS_INPUT
+    const RS_INPUT = 'RS_INPUT';
+    // transaction number
+    const ZEX_VBELN = 'ZEX_VBELN';
+    // ship to party
+    const KUNNR = 'KUNNR';
+    // material
+    const MATNR = 'MATNR';
+    // delivery quantity
+    const LFIMG = 'LFIMG';
+    // batch no.
+    const CHARG = 'CHARG';
+    // location
+    const WERKS = 'WERKS';
+    // fixed value
+    const LFART = 'LFART';
+    const ZEL = 'ZEL';
+    // stoarage
+    const LGORT = 'LGORT';
+    // static value
+    const XABLN = 'XABLN';
+    // current date
+    const WADAT = 'WADAT';
+    // submitted date
+    const WDATU = 'WDATU';
+    // manufacturing date
+    const HSDAT = 'HSDAT';
+    // expiry date
+    const VFDAT = 'VFDAT';
+    // null (already exists in BAPI)
+    const CRATES_IND = 'CRATES_IND';
+    // handling unit (range)
+    const EXIDV = 'EXIDV';
+    // packaging material (pack icon) select allowed material >> (hu) empty or same as vhilm2.
+    const VHILM = 'VHILM';
+    // packing material (always has an entry)
+    const VHILM2 = 'VHILM2';
+    // extras > headers > text
+    const REMARKS = 'REMARKS';
+    const LAST_ITEM_IND = 'LAST_ITEM_IND';
+
+    // PRINTER
+    const PRINTER = 'PRINTER';
+    const ZWI6 = 'ZWI6';
+
+    // ET_PALLETS
+    const ET_PALLETS = 'ET_PALLETS';
+    const ET_PALLETS_W_TO = 'ET_PALLETS_W_TO';
+
+    // export
+    const VBELN = 'VBELN';
+    const OBJECT_LOCKED = 'OBJECT_LOCKED';
+    const NOT_COMPATIBLE = 'NOT_COMPATIBLE';
+    const VOLUME_CAP_ERROR = 'VOLUME_CAP_ERROR';
+    const OTHER_ERROR = 'OTHER_ERROR';
+
+}
