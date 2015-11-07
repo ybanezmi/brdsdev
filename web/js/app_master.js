8e129c2a055088596d27c7510f1fac3b754eef52
JP <jplagahit.dev@gmail.com>
2015-11-08 02:02:40 +0800

    Disable enter key

--- web/js/app_master.js
+++ web/js/app_master.js
@@ -1541,3 +1541,11 @@ $(document).ready(function() {
         e.preventDefault();
     });
 });
+
+$('form').on('keyup keypress', function(e) {
+  var code = e.keyCode || e.which;
+  if (code == 13) { 
+    e.preventDefault();
+    return false;
+  }
+});
